dd7905db8cebfcca52521a7eac5dc4538628edfc
Brian Cheung <bdcheung@gmail.com>
2013-06-01 09:58:55 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dogwalker::Application.config.secret_key_base = 'de1898688c2c4c04a2c9394a9c8e719e24f332a3fb9fee777c1cdf37714d6f5269d4f9d9cdcc06b0763f984ff4a9ee9cf9e971bb0ba93aba2400e7cbd9f8d572'
