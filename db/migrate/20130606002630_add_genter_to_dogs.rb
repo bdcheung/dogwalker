8a8152a0e8f143b92a784aada993e03d1677d86e
Brian Cheung <bdcheung@gmail.com>
2013-06-05 20:27:30 -0400

    Added Gender to the Dog model

---
+++ db/migrate/20130606002630_add_genter_to_dogs.rb
@@ -0,0 +1,5 @@
+class AddGenterToDogs < ActiveRecord::Migration
+  def change
+    add_column :dogs, :gender, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130605233301) do
+ActiveRecord::Schema.define(version: 20130606002630) do
 
   create_table "dogs", force: true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20130605233301) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "fixed"
+    t.string   "gender"
   end
 
 end
