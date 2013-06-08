8e907bceaa23d6ff079a2bef290cdca6186a11a1
Brian Cheung <bdcheung@gmail.com>
2013-06-08 10:49:22 -0400

    Created new walk view and show walk view

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -3,8 +3,25 @@ class WalksController < ApplicationController
 		@walks = Walk.upcoming
 	end
 
+	def new
+		@walk = Walk.new
+	end
+
+	def create
+		@walk = Walk.new(params[:walk])
+		if @walk.save
+			redirect_to @walk
+		else
+			fail
+		end
+	end
+
+	def show
+		@walk = Walk.find(params[:id])
+	end
+
 	private
 	def walk_params
-		params.require(:walk).permit(:starttime, :duration)
+		params.require(:walk).permit(:starttime, :duration_in_minutes, :dog_id)
 	end
 end
